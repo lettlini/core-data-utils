bc3dd4961594c496a50918c1dcef748b0668a452
Kolya Lettl <klettl@posteo.com>
2024-12-22 15:35:32 +0100

    added a 'setup.py' file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="core-data-utils",
+    version="1",
+    author="Kolya Lettl",
+    author_email="kolya.lettl@studserv.uni-leipzig.de",
+    description="framework for working with data structures I frequently use when building data pipelines for research",
+    packages=find_packages(),
+    install_requires=[],
+    extras_require={"image": ["opencv-python"], "graph": ["networkx"]},
+)
