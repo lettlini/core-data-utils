05799f5d4f084d72753745579a80bce5cc6333f9
Kolya Lettl <klettl@posteo.com>
2024-12-28 17:08:31 +0100

    increased version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="core-data-utils",
-    version="1",
+    version="2",
     author="Kolya Lettl",
     author_email="kolya.lettl@studserv.uni-leipzig.de",
     description="framework for working with data structures I frequently use when building data pipelines for research",
